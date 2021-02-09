aeef2d6bf079b087d8cf7e9426ab1e8f1a8035d9
ajaykirtikar <ajaykirtikar2@gmail.com>
2021-02-09 17:58:20 -0500

    added stock stats py file

---
+++ stock_stats/Main.py
@@ -0,0 +1,83 @@
+import yfinance as yf
+import sys 
+
+# demo for using yfinance API to gather info about any stocks entered as an argument
+# argument must be a real stock ticker, case insensitive
+def main():
+    #t = input("Enter a stock ticker: ")
+    stock = yf.Ticker(sys.argv[1])
+
+    # # show actions (dividends, splits)
+    # msft.actions
+
+    # # show dividends  
+    # msft.dividends
+
+    # # show splits
+    # msft.splits
+
+    # # show financials
+    # msft.financials
+    # msft.quarterly_financials
+
+    # # show major holders
+    # msft.major_holders
+
+    # # show institutional holders
+    # msft.institutional_holders
+
+    # # show balance sheet
+    # msft.balance_sheet
+    # msft.quarterly_balance_sheet
+
+    # # show cashflow
+    # msft.cashflow
+    # msft.quarterly_cashflow
+
+    # # show earnings
+    # msft.earnings
+    # msft.quarterly_earnings
+
+    # # show sustainability
+    # msft.sustainability
+
+    # # show ISIN code - *experimental*
+    # # ISIN = International Securities Identification Number
+    # msft.isin
+
+    # # show options expirations
+    # msft.options
+
+    # # get option chain for specific expiration
+    # opt = msft.option_chain('YYYY-MM-DD')
+
+    # data available via: opt.calls, opt.puts
+
+
+    # get stock info
+    info = stock.info
+    print("\nINFO KEYS\n")
+    print(info.keys())
+
+    print("\nSUMMARY\n")
+    print(info['longBusinessSummary'])
+
+    # get historical market data
+    hist = stock.history(period="max")
+    print("\nHISTORY\n")
+    print(hist)
+
+     # show analysts recommendations
+    print("\nRECCOMENDATIONS \n")
+    print(stock.recommendations)
+
+    # show next event (earnings, etc)
+    print("\nEARNINGS\n")
+    print(stock.calendar)
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
