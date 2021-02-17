cf00060b00ba18832299f2340dbc875904ba06cf
Andrew Sanchez <andrews8269@gmail.com>
2021-02-17 16:46:05 -0500

    Added error checking and print statements for debug

--- src/py-service/WatsonNews.py
+++ src/py-service/WatsonNews.py
@@ -16,7 +16,7 @@ def setup():
     return discovery
 
 
-# Run query on Watson News given arguments Query [0], Time[1], Count[2]
+# Run query on Watson News given arguments Query [2], Time[3], Count[4]
 # Time - Range of time to search
 #   Format: NumUnit
 #       Unit: minutes, hours, days, months, or years
@@ -29,59 +29,71 @@ def setup():
 #   Format: Integer
 # Return Value: json dump of all returned from query (May change to be more specifically formated
 def queryStocks(discovery_):
-    discovery = discovery_
-    en_id = "system"
-    passages = True
-    query = sys.argv[1]
-    natural_language_query = query
-    time = sys.argv[2]
-    count = sys.argv[3]
-    highlight = True
-    deduplicate = True
-    filterSearch = "crawl_date>=now-" + time
+    if len(sys.argv) < 3:  # Error check for arguments needed
+        print("ERROR: Invalid Arguments. Use format: 'WatsonNews.py query {query} {time} {count}. Time and Count "
+              "optional")
+    else:
+        print("Query")
+        discovery = discovery_  # Init discovery with passed in object
+        en_id = "system"  # Set environment
+        query = sys.argv[1]  # get query from args
+        natural_language_query = query
+        if len(sys.argv) > 3:  # Check for optional args
+            time = sys.argv[2]
+        else:
+            time = 'years'
+        if len(sys.argv) > 4:
+            count = sys.argv[3]
+        else:
+            count = '1'
+        time = count + time  # Create time filter, default to 1 year
+        filterSearch = "crawl_date>=now-" + time
 
-    print("Searching for articles about " + natural_language_query + " from the last " + time)
+        print("Searching for articles about " + natural_language_query + " from the last " + time)
 
-    response = discovery.query(environment_id=en_id, collection_id='news-en', query=query,
-                               natural_language_query=natural_language_query, passages=True,
-                               count=10, highlight=True, deduplicate=True, filter=filterSearch)
-    print(response)
+        response = discovery.query(environment_id=en_id, collection_id='news-en', query=query,
+                                   natural_language_query=natural_language_query, passages=False,
+                                   count=10, highlight=True, deduplicate=True, filter=filterSearch)  # query Discovery
+        print(response)  # Print response to stdout
 
 
 # Run query on Watson News
 # Return Value: json dump of top 10 most recent articles about the stock market
 def headlines(discovery_):
-    discovery = discovery_
+    if len(sys.argv) < 3:  # Error check for arguments needed
+        print("ERROR: Invalid Arguments. Use format: 'WatsonNews.py headlines {query}")
+    else:
+        print("Headlines")
+        discovery = discovery_
 
-    en_id = "system"
-    passages = True
-    query = "Stocks"
-    natural_language_query = query
-    count = 10
-    highlight = True
-    deduplicate = True
-    filterSearch = "crawl_date>=now-1day"
+        en_id = "system"
+        query = "Stocks"
+        natural_language_query = query
+        qty = 5  # Number of results to return
+        filterSearch = "crawl_date>=now-1day"
 
-    print("Showing top articles for Stocks in the last day")
+        print("Showing top articles for Stocks in the last day")
 
-    response = discovery.query(environment_id=en_id, collection_id='news-en', query=query,
-                               natural_language_query=natural_language_query, passages=False,
-                               count=5, highlight=True, deduplicate=True, filter=filterSearch)
-    out_file = open("news.json", "w")
-    json.dump(response.get_result(), out_file, indent=6, skipkeys=True)
+        response = discovery.query(environment_id=en_id, collection_id='news-en', query=query,
+                                   natural_language_query=natural_language_query, passages=False,
+                                   count=qty, highlight=True, deduplicate=True, filter=filterSearch)  # Query discovery
+        print(response)  # Print to stdout
 
-    out_file.close()
+    # out_file = open("news.json", "w")
+    # json.dump(response.get_result(), out_file, indent=6, skipkeys=True)
+    #
+    # out_file.close()
 
 
 def main():
     discovery = setup()
     print("Main")
     if sys.argv[1] == 'query':
+        print("Calling query")
         queryStocks(discovery)
     elif sys.argv[1] == 'headlines':
         print("Calling headlines")
         headlines(discovery)
-    print("Huh")
 
 
 if __name__ == '__main__':
