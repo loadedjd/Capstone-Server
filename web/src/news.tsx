f0686d3d912037cafa58ca3e41a5e905248cfe86
Andrew Sanchez <andrews8269@gmail.com>
2021-03-09 18:57:52 -0500

    Hello world for react

---
+++ web/src/news.tsx
@@ -0,0 +1,9 @@
+import axios from 'axios';
+import React from 'react';
+export function News() {
+    return (<p>Hello World</p>
+
+    );
+}
+// const articles = (await axios.get('localhost:3000/api/news', 'gamestop')).data
+// const news_element = <news stock='GameStop' articles='GameStop Stock Surging'/>;
