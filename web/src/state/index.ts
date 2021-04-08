adfec508e60a0ee63a3327423982a1f897c0999e
Jared Williams <williamsjared62@gmail.com>
2021-04-08 11:48:04 -0600

    Connected stock graph to state, now shows stock price of selected ticker for the past 1 year

--- web/src/state/index.ts
+++ web/src/state/index.ts
@@ -1,8 +1,11 @@
 import React from 'react';
 import axios, { AxiosRequestConfig } from 'axios';
+import { StockData } from '../models';
+import { getStock } from '../Services/api.service';
 
 export type AppState = {
   ticker: string | undefined;
+  stockData: StockData | undefined;
   setTicker: (ticker: string) => void;
 };
 
@@ -10,9 +13,21 @@ export const AppContext = React.createContext<AppState | undefined>(undefined);
 
 export function useAppState(): AppState {
   const [ticker, setTicker] = React.useState<string | undefined>('TSLA');
+  const [stockData, setStockData] = React.useState<StockData | undefined>(
+    undefined,
+  );
+
+  React.useEffect(() => {
+    console.log('USE EFFECT', ticker);
+
+    getStock(ticker ?? '').then((response) => {
+      setStockData(response.data);
+    });
+  }, [ticker]);
 
   return {
     ticker: ticker,
+    stockData,
     setTicker,
   };
 }
