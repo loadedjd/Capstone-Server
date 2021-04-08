564030d8ea6e3a1d1e60db1b61bf8bbd1cfd5785
Jared Williams <williamsjared62@gmail.com>
2021-04-08 10:33:20 -0600

    Connected state to search bar

--- web/src/SearchBar.tsx
+++ web/src/SearchBar.tsx
@@ -1,46 +1,54 @@
 /* eslint-disable no-use-before-define */
-import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { makeStyles } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography';
-import AppBar from '@material-ui/core/AppBar';
 import { Grid } from '@material-ui/core';
-const drawerWidth = 240;
+import { AppContext } from './state';
+import React from 'react';
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   searchbar: {
     flexGrow: 1,
     padding: theme.spacing(3),
-    marginLeft:960,
-    width:640,
-    backgroundColor: theme.palette.background.default
-  }
+    marginLeft: 960,
+    width: 640,
+    backgroundColor: theme.palette.background.default,
+  },
 }));
 
 export default function SearchBar() {
   const classes = useStyles();
+  const appState = React.useContext(AppContext);
+
   return (
     <Grid
       container
       spacing={1}
-      direction={"column"}
-      justify={"flex-start"}
-      alignItems={"flex-start"}
-      >
+      direction={'column'}
+      justify={'flex-start'}
+      alignItems={'flex-start'}
+    >
       <Grid item>
         <div className={classes.searchbar}>
-         <Autocomplete
+          <Autocomplete
             id="searchbar"
-            size={"medium"}
+            size={'medium'}
             freeSolo
             options={top10Stocks.map((option) => option.title)}
             autoHighlight={true}
             renderInput={(params) => (
-              <TextField {...params} label="Search a stock" margin="normal" variant="outlined" />
+              <TextField
+                {...params}
+                label="Search a stock"
+                margin="normal"
+                variant="outlined"
+                onChange={(event) => {
+                  appState?.setTicker(event.target.value);
+                }}
+              />
             )}
           />
-     </div>
+        </div>
       </Grid>
     </Grid>
   );
@@ -48,14 +56,14 @@ export default function SearchBar() {
 
 // Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
 const top10Stocks = [
-  { title: 'Apple', ticker: 'AAPL'},
-  { title: 'Facebook', ticker: 'FB'},
-  { title: 'Microsoft', ticker: 'MSFT'},
+  { title: 'Apple', ticker: 'AAPL' },
+  { title: 'Facebook', ticker: 'FB' },
+  { title: 'Microsoft', ticker: 'MSFT' },
   { title: 'Gamestop', ticker: 'GME' },
   { title: 'IBM', ticker: 'IBM' },
-  { title: "Tesla", ticker: 'TSLA' },
+  { title: 'Tesla', ticker: 'TSLA' },
   { title: 'Disney', ticker: 'DIS' },
   { title: 'AMC', ticker: 'AMC' },
   { title: 'Google', ticker: 'GOOGL' },
-  { title: 'Netflix', ticker: 'NFLX'},
+  { title: 'Netflix', ticker: 'NFLX' },
 ];
